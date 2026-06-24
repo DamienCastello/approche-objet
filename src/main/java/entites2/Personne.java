9e41a1768eeef4c936b882e5698fdffdc4e21600
Damien Castello <d.castello.13200@gmail.com>
2026-06-24 12:30:58 +0200

    tp3: done

--- src/main/java/entites2/Personne.java
+++ src/main/java/entites2/Personne.java
@@ -3,12 +3,40 @@ package entites2;
 import entites.AdressePostale;
 
 public class Personne {
+    public String nom;
+    public String prenom;
+    public AdressePostale adresse;
+
     Personne(String nom, String prenom, AdressePostale adresse){
         this.nom = nom;
         this.prenom = prenom;
         this.adresse = adresse;
     }
-    public String nom;
-    public String prenom;
-    public AdressePostale adresse;
+
+    void Afficher(){
+        System.out.println(this.nom.toUpperCase() + " " + this.prenom.toUpperCase());
+    }
+
+    void ModifierPrenom(String prenom){
+        this.prenom = prenom;
+    }
+
+    void ModifierAdresse(AdressePostale adresse){
+        this.adresse.numeroRue = adresse.numeroRue;
+        this.adresse.libelleRue = adresse.libelleRue;
+        this.adresse.codePostal = adresse.codePostal;
+        this.adresse.ville = adresse.ville;
+    }
+
+    String RetournerNom(){
+        return this.nom;
+    }
+
+    String RetournerPrenom(){
+        return this.prenom;
+    }
+
+    AdressePostale RetournerAdresse(){
+        return this.adresse;
+    }
 }
--- src/main/java/entites2/TestPersonne.java
+++ src/main/java/entites2/TestPersonne.java
@@ -7,7 +7,11 @@ public class TestPersonne {
         AdressePostale adresse1 = new AdressePostale(5,"rue de la totostory","13200","Totocity");
         AdressePostale adresse2 = new AdressePostale(10,"rue des devs","13200","Yolotown");
 
-        Personne personne1 = new Personne("Castello", "Damien", adresse1);
-        Personne personne2 = new Personne("Mozinor", "Président", adresse2);
+        Personne personne1 = new Personne("castello", "damien", adresse1);
+        Personne personne2 = new Personne("mozinor", "président", adresse2);
+
+
+        AdressePostale adresse3 = new AdressePostale(15,"rue de la république","13200","Arles");
+        personne2.ModifierAdresse(adresse3);
     }
 }
