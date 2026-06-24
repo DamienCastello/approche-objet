a65f6686caee1d69da1dc16cbd88da9d65e0e49b
Damien Castello <d.castello.13200@gmail.com>
2026-06-24 14:09:15 +0200

    tp5: done

---
+++ src/main/java/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package banque.entites;
+
+public class Compte {
+    String numeroCompte;
+    int solde;
+
+    Compte(String numeroCompte, int solde) {
+        this.numeroCompte = numeroCompte;
+        this.solde = solde;
+    }
+}
---
+++ src/main/java/banque/entites/TestBanque.java
@@ -0,0 +1,9 @@
+package banque.entites;
+
+public class TestBanque {
+    public static void main(String[] args) {
+        Compte compte1 = new Compte("123456782468", 777);
+
+        System.out.println(compte1);
+    }
+}
