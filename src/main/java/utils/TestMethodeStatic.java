db945bacc8db84757b585fcc056e4621f49c7968
Damien Castello <d.castello.13200@gmail.com>
2026-06-24 13:50:26 +0200

    tp4: done

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+
+        int number = Integer.parseInt(chaine);
+
+        int a = 1;
+        int b = 2;
+
+        int max = Integer.max(a, b);
+
+        System.out.println(max);
+    }
+}
